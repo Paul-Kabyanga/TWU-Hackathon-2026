737fc8743a7ec7232f3fc3a5e8b7d826a87ae6b7
KaleBale <KabyangaPaul@gmail.com>
2026-03-15 11:05:31 +0300

    Made a bot named dabot and implemented it

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from chess_ui import ChessUI
 # Make sure to import SmartBot from bots.py
 from bots import RandomBot, PacifistBot, HumanPlayer, FreezerBot, CrasherBot, SmartBot
 from secure_bot import SecureBotWrapper
+from Dabot import Dabot
 
 if __name__ == "__main__":
     multiprocessing.freeze_support()
@@ -12,8 +13,9 @@ if __name__ == "__main__":
     print("1. Bot vs Bot (Pacifist vs SmartBot)")
     print("2. Human vs Bot (You vs SmartBot)")
     print("3. Human vs Human")
+    print("4 Bot vs Bot (SmartBot vs Dabot)")
     
-    choice = input("Select Mode (1, 2, or 3): ")
+    choice = input("Select Mode (1, 2, 3 or 4): ")
 
     if choice == "2":
         player1 = HumanPlayer("You")
@@ -24,10 +26,16 @@ if __name__ == "__main__":
         player1 = HumanPlayer("Player1")
         player2 = HumanPlayer("Player2")
         
-    else:
+    elif choice == "1":
         # Default: Watch two bots fight
         player1 = SecureBotWrapper(PacifistBot, "Pacifist Bot")
         player2 = SecureBotWrapper(SmartBot, "Gemini SmartBot")
+
+    elif choice == "4":
+        player1 = SecureBotWrapper(SmartBot, "SmartBot")
+        player2 = SecureBotWrapper(Dabot, "Dabot")
+
+
 
     # Launch the game
     ui = ChessUI(white_bot=player1, black_bot=player2)
